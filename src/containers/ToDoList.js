2a74d9b665cd5026cf299bd3bd32f4ef6443db9a
Misaal <misaal9@gmail.com>
2017-07-19 11:16:46 +0530

    Implemented show by filter

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -2,10 +2,24 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import ToDoList from '../components/ToDoList';
 import { toggleTask } from '../actions';
+import { FILTER } from '../actions';
+
+const getFilteredTodos = (state, filter) => {
+  switch(filter) {
+    case FILTER.SHOW_ALL:
+      return state;
+    case FILTER.SHOW_C:
+      return state.filter(todo => todo.completed);
+    case FILTER.SHOW_IC:
+      return state.filter(todo => !todo.completed);
+    default:
+      return state;
+  }
+};
 
 const mapStateToProps = state => {
   return {
-    todos: state.todos
+    todos: getFilteredTodos(state.todos, state.activeFilter)
   }
 };
 
