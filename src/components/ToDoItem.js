cb5d99ac3a0e34e982566d55dbe06becc7649a64
Misaal <misaal9@gmail.com>
2017-07-19 11:19:44 +0530

    Added dashed border if no items to show

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -4,7 +4,7 @@ const listGroupName = 'list-group-item'
 
 const ToDoItem = ({todo, toggleTask}) => {
   if (!todo) {
-    return <li className="list-group-item not-added">No items added yet</li>;
+    return <li className="list-group-item not-added">No items to show</li>;
   }
   let className = `${listGroupName} ${todo.completed? 'strikethrough' : ''}`;
   return <li className={className} onClick={() => toggleTask(todo)}>{todo.text}</li>;
